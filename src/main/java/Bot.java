0adf855803f43a3ea1abe57aeb9a68729d686d18
Raiuk <vladislav.raiuk@gmail.com>
2018-07-04 16:47:59 +0300

    bot_0.0v

---
+++ src/main/java/Bot.java
@@ -0,0 +1,57 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if(message != null && message.hasText()) {
+            if(message.getText().equals("/help")) {
+                sendMsg(message, "Hi, i'm test bot!");
+            }
+            else
+                sendMsg(message, "I'm test robot");
+        }
+    }
+
+    private  void sendMsg(Message message, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        //sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(s);
+
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "SelecterBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "614545720:AAEPKyxx0t_Sc4Luo5I_77gH_xU1LGc5hsY";
+    }
+}
